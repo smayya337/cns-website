a3dae186fe7cf95c5e14d0520f22b347d3a7dde5
Shreyas Mayya <smayya337@gmail.com>
2024-08-02 16:28:03 -0400

    fix app name in admin interface

---
+++ acm_website/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "acm_website.apps.FrontendConfig"
---
+++ acm_website/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FrontendConfig(AppConfig):
+    name = "acm_website"
+    verbose_name = "ACM @ UVA Website"
